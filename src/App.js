e2a7eb631bc70185c37c8b3830f781ca172eb486
Raymondkingjnr <raymondnnaji660@gmail.com>
2022-12-01 09:08:02 +0100

    modified:   public/index.html 	modified:   src/App.js 	new file:   src/Image/HydroHat.png 	new file:   src/Image/patwatch.png 	modified:   src/Image/pgru.png 	new file:   src/Image/pgru1.png 	modified:   src/components/Navcomponenets/Nav.css 	modified:   src/components/herocomponents/Hero.css 	modified:   src/components/herocomponents/Hero.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Nav from "./components/Navcomponenets/Nav.js";
 import Hero from "./components/herocomponents/Hero.js";
 import "./App.css";
 import image from "./Image/pgru.png";
+import cap from "./Image/HydroHat.png";
+import wrist from "./Image/patwatch.png";
 import { watch } from "./Data/data";
 import { shoe } from "./Data/data";
 import { pants } from "./Data/data";
@@ -11,7 +13,7 @@ function App() {
   return (
     <div>
       <Nav watch={watch} shoe={shoe} pants={pants} />
-      <Hero image={image} />
+      <Hero image={image} cap={cap} wrist={wrist} />
     </div>
   );
 }
--- src/components/herocomponents/Hero.js
+++ src/components/herocomponents/Hero.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Hero.css";
-function Hero({ image }) {
+function Hero({ image, cap, wrist }) {
   return (
     <section className="hero">
       <main className="hero--section">
@@ -10,12 +10,30 @@ function Hero({ image }) {
           <h3>Tommy-Hilfiger, 2021</h3>
           <h4>Colten Kell Jacket</h4>
           <p className="border--bottom"></p>
-          <button className="btn--hero">Shop</button>
+          <button className=" btn btn--hero">Shop</button>
         </div>
         <div className="baner">
           <img src={image} alt="" />
         </div>
       </main>
+      <article className="down">
+        <div className="bg-cap">
+          <div className="inner-text-cap">
+            <h3>KELEWASCO ESP</h3>
+            <p>customized to fit your style</p>
+            <button className="btn btn-cap">shop now</button>
+          </div>
+          <img src={cap} alt="" />
+        </div>
+        <div className="bg-watch">
+          <img src={wrist} alt="" />
+          <div className="inner-text-watch">
+            <h3>OKAWAJ VAL</h3>
+            <p>more style in your box to unfold</p>
+            <button className="btn btn-watch">shop now</button>
+          </div>
+        </div>
+      </article>
     </section>
   );
 }
